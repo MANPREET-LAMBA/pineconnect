4073bf1508e8715e2ac5aaea05a97cea09db015a
Apple <apple@Apples-MacBook-Air.local>
2026-07-08 00:16:44 +0530

    Resolved merge conflicts 3

--- BridegeFrontend/src/component/Login.jsx
+++ BridegeFrontend/src/component/Login.jsx
@@ -61,7 +61,7 @@ export default function Login() {
         }
       );
 
-      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("token", response.token);
 
       navigate("/")
 
